a7f8361bc614b25afa9fcd4d616e602c65888ca7
Aabesov <aabesov2005@mail.ru>
2022-12-26 08:48:26 +0600

    created CRUD

--- meal/urls.py
+++ meal/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexPageView.as_view(), name="index"),
+    path("update/<int:pk>/", views.edit, name="update_food"),
+    path("delete/<int:pk>/", views.delete),
     # path('inner/', views.InnerPageView.as_view(), name="inner"),
-
+    path("create/", views.create),
 ]
--- meal/views.py
+++ meal/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
-
+from .models import Food
 from meal.models import Food
+from django.http import HttpResponseRedirect, HttpResponseNotFound
 
 
 class IndexPageView(generic.ListView):
-    # model = Meal
     template_name = "index.html"
     context_object_name = 'index'
 
@@ -14,6 +14,40 @@ class IndexPageView(generic.ListView):
         return queryset
 
 
-# class InnerPageView(generic.TemplateView):
-#     template_name = "sample-inner-page.html"
-#     context_object_name = 'inner'
+def create(request):
+    if request.method == "POST":
+        person = Food()
+        person.name = request.POST.get("name")
+        person.ingredients = request.POST.get("ingredients")
+        person.picture = request.POST.get("picture")
+        person.price = request.POST.get("price")
+        person.category = request.POST.get("category")
+        person.save()
+    return HttpResponseRedirect("/")
+
+
+def edit(request, pk):
+    try:
+        person = Food.objects.get(id=pk)
+
+        if request.method == "POST":
+            person.name = request.POST.get("name")
+            person.ingredients = request.POST.get("ingredients")
+            person.picture = request.POST.get("picture")
+            person.price = request.POST.get("price")
+            person.category = request.POST.get("category")
+            person.save()
+            return HttpResponseRedirect("/")
+        else:
+            return render(request, "update_food.html", {"person": person})
+    except Food.DoesNotExist:
+        return HttpResponseNotFound("<h2>Person not found</h2>")
+
+
+def delete(request, pk):
+    try:
+        person = Food.objects.get(id=pk)
+        person.delete()
+        return HttpResponseRedirect("/")
+    except Food.DoesNotExist:
+        return HttpResponseNotFound("<h2>Person not found</h2>")
